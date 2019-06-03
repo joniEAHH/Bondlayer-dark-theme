aa7e236df65458d172220d0344ae8558befd1182
Joni Dores <joni.dores@gmail.com>
2019-06-03 17:42:54 +0100

    first commit

---
+++ bl-dark-theme.js
@@ -0,0 +1,15 @@
+window.addEventListener('DOMNodeRemoved', function() {
+  if (document.getElementById('bl-dark-theme') == null) {
+    var head = document.getElementsByTagName('head')[0],
+      link = document.createElement('link');
+
+    link.id     = 'bl-dark-theme';
+    link.rel    = 'stylesheet';
+    link.type   = 'text/css';
+    link.href   = chrome.extension.getURL('bl-dark-theme.css');
+    link.media  = 'all';
+
+    head.appendChild(link);
+  }
+});
+
